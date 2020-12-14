164c3ede1a40f96c5e1fa3541ad7d2e80e9983d0
Dave Proffer <dproffer@acm.org>
2020-12-13 17:04:29 -0800

    Shell script to keep sensor program running after some errors occur

---
+++ hack_ble.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+trap "echo Exited!; exit;" SIGINT SIGTERM
+
+if [ $EUID -ne 0 ]; then
+    echo "This script should be run as root." > /dev/stderr
+    exit 1
+fi
+
+while :
+do
+    echo 'Running ble_sensor_mqtt_pub'
+    date '+%Y%m%d%H%M%S'
+	echo "Press [CTRL+C] to stop.."
+    cd /home/pi/atc-govee
+	/home/pi/atc-govee/ble_sensor_mqtt_pub 0 1 200 500
+done
+
+
